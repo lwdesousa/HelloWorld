4066438f1276c069e12ffbe373740117baf7b3aa
Leticia Witkowsky De Sousa <lwdesousa@usf.edu>
2020-01-23 13:01:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause To Display Hello World!
+            Console.WriteLine("Press Any Key To Continue");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
